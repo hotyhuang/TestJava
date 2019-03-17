a2c7ce273fde465b6b701046dfcf2649cc731437
Haitian Huang <hotyhuang@gmail.com>
2019-03-17 13:46:37 -0400

    Test ssh

--- LearnJava/src/play/Game.java
+++ LearnJava/src/play/Game.java
@@ -5,7 +5,7 @@ import learn.dota.Hero;
 public class Game {
 
 	public static void main(String[] args) {
-		Hero aa = new Hero("Earth Shaker");
+		Hero aa = new Hero("Hey dude");
 		System.out.println(aa.name);
 	}
 
